343052cc77805645f2db55cf026a10a4906814ee
hawau <hawaubaraje@gmail.com>
2025-04-26 23:54:22 +0100

    new file:   instance/database.db 	modified:   website/__init__.py 	modified:   website/__pycache__/__init__.cpython-313.pyc 	new file:   website/__pycache__/auth.cpython-313.pyc 	new file:   website/__pycache__/models.cpython-313.pyc 	modified:   website/__pycache__/views.cpython-313.pyc 	modified:   website/auth.py 	modified:   website/models.py 	new file:   website/static/images/cats1.jpg 	new file:   website/static/images/dogs1.jpg 	modified:   website/templates/base.html 	modified:   website/templates/login.html 	new file:   website/templates/sign_up.html 	modified:   website/views.py

---
+++ website/models.py
@@ -0,0 +1,11 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True,nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+   
--- website/views.py
+++ website/views.py
@@ -1,26 +1,22 @@
 from flask import Blueprint, render_template
+from flask_login import  login_required,  current_user
 
 views = Blueprint('views', __name__)
 
-# Sample data
-pets = [
-    {"id": 1, "name": "Buddy", "type": "Dog", "age": 3},
-    {"id": 2, "name": "Mittens", "type": "Cat", "age": 2}
-]
+
 
 @views.route('/')
 def home():
-    return render_template("home.html", pets=pets)
+    return render_template("home.html")
 
-@views.route('/pet/<int:pet_id>')
-def pet_detail(pet_id):
-    pet = next((p for p in pets if p["id"] == pet_id), None)
-    return render_template("pet_detail.html", pet=pet)
 
 @views.route('/add')
+@login_required
 def add_pet():
     return render_template("add_pet.html")
 
-@views.route('/login')
-def login():
-    return render_template("login.html")
+
+
+
+
+
